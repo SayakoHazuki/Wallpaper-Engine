18d5b56131819de3b180f7aaebab590de908fdf3
Kai Kamf <74895715+Kai9073@users.noreply.github.com>
2022-07-08 23:17:13 +0800

    Smoother animation

--- main.py
+++ main.py
@@ -11,8 +11,10 @@ tk.attributes('-fullscreen', True)
 
 canvasX = int(W*1.15)
 canvasY = int(H*1.15)
+canvasXOffset = 0-W*0.075
+canvasYOffset = 0-H*0.075
 canvas = tkinter.Canvas(tk, width=canvasX, height=canvasY, bd=-2)
-canvas.place(x=0-W*0.115, y=0-H*0.075)
+canvas.place(x=canvasXOffset, y=canvasYOffset)
 
 imagePath = "./background.jpg"
 originalImage = Image.open(imagePath)
@@ -25,7 +27,9 @@ canvas.create_image(canvasX/2, canvasY/2, image=img, anchor=tkinter.CENTER)
 
 
 def updateCanvasPos():
-    global canvas
+    global canvasXOffset, canvasYOffset
+    originalX = canvasXOffset 
+    originalY = canvasYOffset 
     cursorPos = pyautogui.position()
     canvasXOffset = int(0 - (cursorPos.x * 0.15))
     canvasYOffset = int(0 - (cursorPos.y * 0.15))
@@ -33,7 +37,8 @@ def updateCanvasPos():
     canvasXOffset = max(W*-0.15 + 2, canvasXOffset)
     canvasYOffset = min(-2, canvasYOffset)
     canvasYOffset = max(H*-0.15 + 2, canvasYOffset)
-    print(canvasXOffset,canvasYOffset)
+    canvasXOffset = 0.85 * originalX + 0.15 * canvasXOffset
+    canvasYOffset = 0.85 * originalY + 0.15 * canvasYOffset
     canvas.place(x=canvasXOffset, y=canvasYOffset)
     tk.after(1, updateCanvasPos)
 
